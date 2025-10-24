d211638bac8374b14537064688fb567efb53c183
Claus Stadler <RavenArkadon@googlemail.com>
2025-10-24 12:15:21 +0200

    updated osm 15min city script

--- openstreetmap/15min-city/osmium-15min-city.sh
+++ openstreetmap/15min-city/osmium-15min-city.sh
@@ -1,31 +1,48 @@
-#!/bin/bash
+#!/bi"${TYPE}"bash
+
+# Osm entity type (node, way, relation)
+TYPE=""
+# TYPE="n/"
 
 args=(
   # 1. residential
-  # building=residential,apartments,house,detached,terrace
+  # "${TYPE}"building=residential,apartments,house,detached,terrace
 
   # 2. shopping / retail
-  shop=supermarket,convenience,greengrocer,bakery,butcher,dairy
-  shop=chemist,cosmetics amenity=pharmacy
+  "${TYPE}"shop=supermarket,convenience,greengrocer,bakery,butcher,dairy
+  "${TYPE}"shop=chemist,cosmetics
+  "${TYPE}"amenity=pharmacy
 
   # 3. health care
-  amenity=doctors,dentist,hospital healthcare=hospital
-  social_facility=nursing_home amenity=social_facility
+  "${TYPE}"amenity=doctors,dentist,hospital
+  "${TYPE}"healthcare=hospital
+  "${TYPE}"social_facility=nursing_home
+  "${TYPE}"amenity=social_facility
 
   # 4. education & child care
-  amenity=kindergarten,childcare,school,college,university,library
+  "${TYPE}"amenity=kindergarten,childcare,school,college,university,library
 
   # 5. Work
-  amenity=coworking_space
+  "${TYPE}"amenity=coworking_space
 
   # 6. Leisure, Sport & Culture
-  leisure=park
-  amenity=cinema,theatre
-  amenity=restaurant,cafe,bar,fast_food,pub
+  "${TYPE}"leisure=park
+  "${TYPE}"amenity=cinema,theatre
+  "${TYPE}"amenity=restaurant,cafe,bar,fast_food,pub
 
   # 7. Public Transport
-  highway=bus_stop public_transport=platform railway=station public_transport=stop_position amenity=bus_station railway=halt light_rail=station subway=station tram=station
+  "${TYPE}"highway=bus_stop
+  "${TYPE}"public_transport=platform
+  "${TYPE}"railway=station
+  "${TYPE}"public_transport=stop_position
+  "${TYPE}"amenity=bus_station
+  "${TYPE}"railway=halt
+  "${TYPE}"light_rail=station
+  "${TYPE}"subway=station
+  "${TYPE}"tram=station
 )
 
-osmium tags-filter "$1" "${args[@]}" -o out.osm.pbf
+echo "Args: ${args[@]}"
+
+osmium tags-filter "$1" "${args[@]}" -o out.nodes.osm.pbf
 
